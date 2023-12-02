649d30c8b5d4ed4f11527ede895524bac8c42ba1
Raell Dottin <raell.dottin@gmail.com>
2023-12-01 20:20:43 -0500

    Refactor calibration script to improve number extraction and conversion logic

--- day_1/calibration.py
+++ day_1/calibration.py
@@ -12,27 +12,79 @@ class Calibration(Input):
         super().__init__()
         self.values = []
 
+    def convert(self, s: str) -> str:
+        # print(f"{s=}")
+        if s == "one":
+            s = "1"
+        elif s == "two":
+            s = "2"
+        elif s == "three":
+            s = "3"
+        elif s == "four":
+            s = "4"
+        elif s == "five":
+            s = "5"
+        elif s == "six":
+            s = "6"
+        elif s == "seven":
+            s = "7"
+        elif s == "eight":
+            s = "8"
+        elif s == "nine":
+            s = "9"
+        # print(f"{s=}")
+
+        return s
+
     def calibrate(self, s: str) -> int:
+        numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        numbers.extend(
+            ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+        )
+        first_digit = "0"
+        last_digit = "0"
+
         i = 0
-        numbers = [str(i) for i in range(1, 9)]
-        first_digit = 0
-        last_digit = 0
+        match = False
         while i < len(s):
-            if s[i : i + 1] in numbers:
-                first_digit = s[i : i + 1]
+            for number in numbers:
+                if s[i : i + len(number)] == number:
+                    first_digit = s[i : i + len(number)]
+                    match = True
+                    break
+
+            if match:
                 break
+
             i = i + 1
 
+        match = False
+        match_index = i + 1
         i = len(s)
         while i > 0:
-            if s[i - 1 : i] in numbers:
-                last_digit = s[i - 1 : i]
+            for number in numbers:
+                if s[i - len(number) : i] == number:
+                    last_digit = s[i - len(number) : i]
+                    match = True
+                    break
+            if match:
                 break
+
             i = i - 1
 
+        first_digit = self.convert(first_digit)
+        last_digit = self.convert(last_digit)
+
+        # I don't know if missing values is considered 0.
+        # value = (
+        #    int(f"{first_digit}{last_digit}")
+        #    if last_digit != "0"
+        #    else int(f"{first_digit}")
+        # )
         value = int(f"{first_digit}{last_digit}")
-        print(f"{s=}{value=}")
-        return int(f"{first_digit}{last_digit}")
+        print(f"String: {s}")
+        print(f"Calibration value: {value}")
+        return value
 
 
 def main():
@@ -40,11 +92,11 @@ def main():
 
     c = 1
     for s in solution.document:
-        print(f"Line: {c}")
+        print(f"Line Number: {c}")
         solution.values.append(solution.calibrate(s))
         c = c + 1
 
-    print(sum(solution.values))
+    print(f"Sum of all calibration values is {sum(solution.values)}")
 
 
 if __name__ == "__main__":
