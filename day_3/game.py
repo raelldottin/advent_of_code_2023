3c46eb27d2e5187489f29eb3d79db58077bf1b5a
Raell Dottin <raell.dottin@gmail.com>
2023-12-18 18:29:26 -0500

    change logic for checking for symbols

--- day_3/game.py
+++ day_3/game.py
@@ -11,45 +11,60 @@ class Solution(Input):
     def __init__(self):
         super().__init__()
 
-    def is_symbol_except_dot_or_digit(self, variable):
-        return  variable != '.' and not variable.isdigit()
+    def is_adjacent(self, data, line_index, char_index, delta):
+        """
+        Check if the character at a specific position is not '.' and not a digit.
+        """
+        new_index = char_index + delta
+        if 0 <= new_index < len(data[line_index]):
+            char = data[line_index][new_index]
+            return char != "." and not char.isdigit()
+        return False
+
+    def adjacency(self, data, line_index, character_index) -> bool:
+        # Check left and right on the current line
+        if self.is_adjacent(data, line_index, character_index, -1) or \
+           self.is_adjacent(data, line_index, character_index, 1):
+            return True
+
+        # Check the previous line
+        if line_index > 0:
+            for delta in (-1, 0, 1):
+                if self.is_adjacent(data, line_index - 1, character_index, delta):
+                    return True
+
+        # Check the next line
+        if line_index < len(data) - 1:
+            for delta in (-1, 0, 1):
+                if self.is_adjacent(data, line_index + 1, character_index, delta):
+                    return True
+
+        return False
+
 
     def solve(self):
+        sum = 0
         for line_index, line in enumerate(self.data):
             future_index = 0
+            number = []
+            match = False
             for character_index, character in enumerate(line):
-                if future_index > 0 and character_index <= future_index:
-                    continue
-                number = []
-                if character.isdigit():
-                    number.append(character)
-                    print(f"{character=}")
-                    while (character_index + 1) < len(line):
-                        character_index = character_index + 1
-                        if line[character_index].isdigit():
-                            number.append(line[character_index])
-                            future_index = character_index
-                        else:
-                            break
-                    print(f"Line {line_index + 1}: {number=}")
-                    continue
-                if (character_index - 1) >= 0 and ((character_index - 1) < len(line)):
-                    if (self.data[line_index][character_index - 1] != ".") and (not self.data[line_index][character_index - 1].isdigit()):
-                        print(f"Match on line {line_index} and character {character_index - 1}: {self.data[line_index][character_index - 1]} {self.data[line_index][character_index]}")
-                        continue
-                if (character_index + 1) >= 0 and ((character_index + 1) < len(line)):
-                    if (self.data[line_index][character_index + 1] != ".") and (not self.data[line_index][character_index + 1].isdigit()):
-                        print(f"Match on line {line_index} and character {character_index + 1}: {self.data[line_index][character_index + 1]} {self.data[line_index][character_index]}")
-                        continue
-                #if (line[line_index][character_index] != ".") and (not line[character_index].isdigit()):
-                #    print(f"Match on line {character_index}: {line[character_index]}")
-                #if line_index - 1 >= 0 and line_index - 1 < len(self.data):
-                #    if (character_index - 1) >= 0 and character_index - 1 < len(line):
-                #        if (line[character_index - 1] != ".") and (not line[character_index - 1].isdigit()):
-                #            print(f"Match on line {character_index - 1}: {line[character_index - 1]}")
-                #    if (character_index + 1) >= 0 and character_index + 1 < len(line):
-                #        if (line[character_index + 1] != ".") and (not line[character_index + 1].isdigit()):
-                #            print(f"Match on line {character_index + 1}: {line[character_index + 1]}")
+                if match and not line[character_index].isdigit():
+                    print(f"Line {line_index + 1}: {character=} {number=} {match=}")
+                    sum = sum + int(''.join(number))
+                    number = []
+                    match = False
+                if line[character_index].isdigit():
+                    prev_match = match
+                    match = self.adjacency(self.data, line_index, character_index)
+                    if not match and prev_match:
+                        match = prev_match
+                    number.append(line[character_index])
+                    future_index = character_index
+                    character_index = character_index + 1
+
+        print(f"Sum: {sum}")
+
 def main() -> None:
     answer = Solution()
     answer.solve()
